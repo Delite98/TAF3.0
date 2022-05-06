9338648ff40d0d0c7e455347bcf652687c8d151c
Darya <dashkash22@mail.ru>
2022-05-06 22:26:40 +0300

    Lesson

--- src/main/java/services/BrowsersService.java
+++ src/main/java/services/BrowsersService.java
@@ -6,44 +6,50 @@ import io.github.bonigarcia.wdm.config.DriverManagerType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.Locale;
+import java.time.Duration;
 
 public class BrowsersService {
     private WebDriver driver = null;
-    private DriverManagerType driverManagerType = null;
 
     public BrowsersService() {
-        switch (ReadProperties.browserName().toLowerCase(Locale.ROOT)){
+        switch (ReadProperties.browserName().toLowerCase()) {
             case "chrome":
-                driverManagerType = DriverManagerType.CHROME;
+                DriverManagerType driverManagerType = DriverManagerType.CHROME;
                 WebDriverManager.getInstance(driverManagerType).setup();
 
-                driver = new ChromeDriver(getChromeOptions());
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.setHeadless(ReadProperties.isHeadless());
+                chromeOptions.addArguments("--disable-gpu");
+                //chromeOptions.addArguments("--window-size=1920,1200");
+                chromeOptions.addArguments("--ignore-certificate-errors");
+                chromeOptions.addArguments("--silent");
+                chromeOptions.addArguments("--start-maximized");
+
+                driver = new ChromeDriver(chromeOptions);
+
                 break;
             case "firefox":
+                driverManagerType = DriverManagerType.FIREFOX;
+                WebDriverManager.getInstance(driverManagerType).setup();
+
+                driver = new FirefoxDriver();
                 break;
             default:
-                System.out.println("Browser " + ReadProperties.browserName()+ " is not supported.");
+                System.out.println("Browser " + ReadProperties.browserName() + " is not supported.");
+                break;
         }
-
     }
-    public WebDriver getDriver(){
-        driver.manage().window().maximize();
+
+    public WebDriver getDriver() {
+        //driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        return this.driver;
-    }
-    public ChromeOptions getChromeOptions(){
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setHeadless(ReadProperties.isHeadless());
-        chromeOptions.addArguments("--disable-gpu");
-        //chromeOptions.addArguments("--window-size=1920,1200");
-        chromeOptions.addArguments("--ignore-certificate-errors");
-        chromeOptions.addArguments("--silent");
-        chromeOptions.addArguments("--start-maximized");
-        chromeOptions.addArguments("--incognito");
+        //driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(60));
 
-        return chromeOptions;
-
+        return driver;
     }
 }
