3b133a15351b4ab7bdb90f6e91b9f73d874a6697
Darya <dashkash22@mail.ru>
2022-06-20 22:01:24 +0300

    Jenkins Lesson

--- src/test/java/tests/SelenideTest.java
+++ src/test/java/tests/SelenideTest.java
@@ -37,128 +37,8 @@ public class SelenideTest {
         Configuration.reportsFolder = "target/";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.pageLoadTimeout = 3000;
-        //Configuration.pageLoadTimeout = Duration.ofSeconds(30).toMillis();
+        Configuration.pageLoadTimeout = Duration.ofSeconds(30).toMillis();
         Configuration.timeout = 10;
-//
-//        // or for fine-tuning:
-//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
-//                .screenshots(false)
-//                .savePageSource(true)
-//        );
-
-    }
-
-    @Test
-    public void simpleSelenideTest(){
-        open(ReadProperties.getUrl());
-
-        $(By.id("name")).setValue(ReadProperties.username());
-        $("#password").setValue(ReadProperties.password());
-        $("#button_primary").click();
-
-        $(".page_title").shouldBe(visible).shouldHave(text("All Projects1"));
-
-    }
-
-    @Test
-    public void simpleSelenideTest1(){
-        open("/");
-
-        $(By.id("name")).setValue(ReadProperties.username());
-        $("#password").setValue(ReadProperties.password());
-        $("#button_primary").click();
-
-        $(".page_title").shouldBe(visible).shouldHave(text("All Projects"));
-        $$("div.project")
-                .filter(visible)
-                .shouldHave(size(26))
-                .find(text("erg"))
-                .click();
-        $$("div.project")
-                .first();
-        $$("div.project")
-                .get(2);
-
-
-        sleep(1000);
-        refresh();
-        title();
-        executeJavaScript("");
-    }
-
-    @Test
-    public void selenideTest2() throws FileNotFoundException {
-        open(ReadProperties.getUrl());
-
-        $(By.id("name")).setValue(ReadProperties.username());
-        $("#password").setValue(ReadProperties.password());
-        $("#button_primary").click();
-
-        $(byText("asdasdasdfaszc")).innerText();
-        $(byText("asdasdasdfaszc")).getText();
-        $(byText("asdasdasdfaszc")).innerHtml();
-        $(byText("asdasdasdfaszc")).data("");
-        $(byText("asdasdasdfaszc")).val();
-        $(byText("asdasdasdfaszc")).getValue();
-        $(byText("asdasdasdfaszc")).scrollTo();
-        $(byText("asdasdasdfaszc")).doubleClick();
-        $(byText("asdasdasdfaszc")).contextClick();
-        $(byText("asdasdasdfaszc")).hover();
-        $(byText("asdasdasdfaszc")).find(By.xpath(""));
-        $(byText("asdasdasdfaszc")).closest("tr");
-        $(byText("asdasdasdfaszc")).ancestor("div");
-        $(byText("asdasdasdfaszc")).sibling(1);
-        $(byText("asdasdasdfaszc")).parent();
-        $(byText("asdasdasdfaszc")).uploadFile(new File(""));
-        //$(byText("asdasdasdfaszc")).download();
-
-
-
-
-
-    }
-
-    @Test
-    public void selenideTest3 () {
-        open("/");
-
-        $(By.id("name")).setValue(ReadProperties.username());
-        $("#password").setValue(ReadProperties.password());
-        $("#button_primary").click();
-
-        open("/index.php?/admin/projects/overview");
-        $$(By.className("hoverSensitive"))
-                .shouldHave(size(26))
-                .find(text("erg"))
-                .find(By.tagName("a"))
-                .click();
-        $("#announcement")
-                .should(exist)
-                .shouldBe(visible)
-                .shouldHave(exactText("This is the description for the project1"));
-        $("#announcement")
-                .should(exist)
-                .should(hidden).should(disappear).shouldNotBe(visible)
-                .shouldBe(readonly)
-                .shouldHave(name("fnama"))
-                .shouldHave(value("John"))
-                .shouldHave(type("checkbox"))
-                .shouldBe(empty)
-                .shouldBe(focused);
-        WebElement webElement = $("#announcement").toWebElement();
-
-        Condition clickable = and("can be clicked", visible, enabled);
-        $$("#announcement")
-                .findBy(clickable).click();
-
-        $("#announcement").shouldBe(clickable);
-
-        $("#announcement")
-                .shouldHave(text("Expected Text")) //содержит текст
-                .shouldHave(matchText("Expected Text")) //regex
-                .shouldHave(exactText("Expected Text")) //проверка без учета регистра
-                .shouldHave(textCaseSensitive("Expected Result Text"))// содержит текст с учетом регистра
-                .shouldHave(exactTextCaseSensitive("Expected Result Text"));  //проверка с учетом регистра
 
     }
 
@@ -172,7 +52,7 @@ public class SelenideTest {
         loginPage.psw.setValue(ReadProperties.password());
         loginPage.loginButton.click();
 
-        $(".page_title").shouldBe(visible).shouldHave(text("All Projects1"));
+        $(".page_title").shouldBe(visible).shouldHave(text("All Projects"));
 
     }
 }
